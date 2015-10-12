3d9fac69093589010e7f68732454558d1f0f6849
shaniavnaim <shaniavnaim@gmail.com>
2015-10-12 16:41:10 +0300

    blah

---
+++ WebAppFinalProj/Models/Instructor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebAppFinalProj.Models
+{
+    public class Instructor
+    {
+        [Key]
+        public int InstructorId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        public string PhoneNum { get; set; }
+
+        public ICollection<Course> Courses { get; set; }
+    }
+} 
